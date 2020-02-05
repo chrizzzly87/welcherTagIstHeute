fe4a91f3e4939fd5ba74717da954e9612d11fa29
Christian.Wisniewski <christian.wisniewski@statista.com>
2020-02-05 10:43:09 +0100

    Added caching

--- index.js
+++ index.js
@@ -14,14 +14,32 @@ app.get("/", (req, res, next) => {
     res.send('läuft');
 });
 
+function formatDate(date) {
+    let month = ('0' + (date.getMonth() + 1)).substring(-2);
+    let day = ('0' + date.getDate()).substring(-2);
+
+    return `${month}${day}`;
+}
+
+let cache = {};
+
 app.get("/no-not-na-day-n", (req, res, next) => {
     const route = 'https://welcher-tag-ist-heute.org';
     let days = [];
+    let startTime = new Date();
+    // get current day to check if it's already cached
+    let currentDay = formatDate(Date.now());
+    if (currentDay in cache) {
+        return cache[currentDay];
+    }
+
+
     fetch(route)
         .then(res => res.text())
         .then(body => {
             let parsedBody = HTMLParser.parse(body);
             let slides = parsedBody.querySelectorAll('.slides a');
+            let result = [];
             let days = [];
             if (slides) {
                 slides.forEach(slide => {
@@ -29,7 +47,14 @@ app.get("/no-not-na-day-n", (req, res, next) => {
                     let description = slide.querySelector('p.text').innerHTML.trim();
                     days.push({title: title, description: description});
                 });
-            }            
-            res.json(days).end();
+            }
+            let totalExecutionTime = new Date() - startTime;
+            result = {
+                executionTime: totalExecutionTime,
+                result: days,
+            },
+            // save to cache
+            cache[currentDay] = result;      
+            res.json(result).end();
         });
 });
